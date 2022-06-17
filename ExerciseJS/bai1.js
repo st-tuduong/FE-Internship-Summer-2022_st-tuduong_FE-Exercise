f0f246400ab38e5c64d232c0ac85a62c7c6c153a
st-tuduong <tu.duong@supremetech.vn>
2022-06-17 09:16:31 +0700

    remove Exercise1

---
+++ ExerciseJS/bai1.js
@@ -0,0 +1,18 @@
+function Sum(a,b) {
+
+    // option1
+    // if(a>b){
+    //    return (a + b);
+    // }
+    // else{
+    //     return ((a + b) * 3);
+    // }
+
+    // option2 Recommend
+    if(a>b){
+       return (a + b);
+    }
+        return ((a + b) * 3);
+    }
+
+console.log(Sum(6,4));
---
+++ ExerciseJS/bai2.js
@@ -0,0 +1,11 @@
+function Array(string) {
+    var sum = 0;
+    for(i = 0; i < string.length; i++) {
+        // if(string === '*') {
+        //     // sum = sum + (+[string]);
+        //     console.log(+(string));
+        // }
+    }
+}
+
+console.log(Array('1*4'));
