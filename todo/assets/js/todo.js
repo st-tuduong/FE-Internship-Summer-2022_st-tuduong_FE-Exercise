51ced42f01cfa9ae4774f071d54d0098d47ed018
st-tuduong <tu.duong@supremetech.vn>
2022-06-23 22:59:28 +0700

    Fix ToDo

--- todo/assets/js/todo.js
+++ todo/assets/js/todo.js
@@ -1,22 +1,4 @@
 var listTodo = [];
-var btnAdd = document.querySelector('.js-btn-add')
-btnAdd.addEventListener('click', function(e) {
-  addTodo();
-})
-
-function addTodo() {
-  var getContent = document.querySelector('.text-todo').value;
-  var getStorageItem = localStorage.getItem('todos');
-  var listTodo = JSON.parse(getStorageItem) || [];
-  if (listTodo) {
-    listTodo.push({
-      text: getContent,
-      date: Date.now()
-    });
-  }
-  localStorage.setItem('todos', JSON.stringify(listTodo))
-  renderTodo();
-}
 
 function renderTodo() {
   var getListToDo = JSON.parse(localStorage.getItem('todos'));
@@ -30,28 +12,41 @@ function renderTodo() {
     })
     document.querySelector('.list-todo').innerHTML = html;
   }
+  var btnAdd = document.querySelector('.js-btn-add')
+    btnAdd.addEventListener('click', function(e) {
+      addTodo();
+    })
+    var btnDelete = document.querySelectorAll('.js-btn-delete')
+      btnDelete.forEach(function(item){
+        item.addEventListener('click', function(e) {
+          removeTodo(e.target.id);
+        })
+      })    
 }
-renderTodo()
 
-var btnDelete = document.querySelectorAll('.js-btn-delete')
-btnDelete.forEach(function(item){
-  item.addEventListener('click', function(e) {
-    removeTodo(e.target.id)
-  })
-})
-
+function addTodo() {
+  var getContent = document.querySelector('.text-todo').value;
+  var getStorageItem = localStorage.getItem('todos');
+  var listTodo = JSON.parse(getStorageItem) || [];
+  if (listTodo) {
+    listTodo.push({
+      date: Date.now(),
+      text: getContent
+    });
+    localStorage.setItem('todos', JSON.stringify(listTodo));
+    renderTodo();
+  }
+}
 
 function removeTodo(id) {
   var getStorageItem = localStorage.getItem('todos');
   var listTodo = JSON.parse(getStorageItem) || [];
   listTodo.map(function(key){
     if(key.date === +id) {
-      delete key
+      listTodo.splice(key, 1);
     }
   })
-  localStorage.setItem('todos', JSON.stringify(listTodo))
+  localStorage.setItem('todos', JSON.stringify(listTodo));
   renderTodo();
 }
-
-
-
+renderTodo();
