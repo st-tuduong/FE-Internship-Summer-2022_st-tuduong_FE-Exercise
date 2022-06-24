52ec2749763aac384170e60259698a25344588fd
st-tuduong <tu.duong@supremetech.vn>
2022-06-24 13:18:55 +0700

    Fix ToDo

--- todo/assets/js/todo.js
+++ todo/assets/js/todo.js
@@ -1,56 +1,60 @@
-var listTodo = [];
-
 var btnAdd = document.querySelector('.js-btn-add')
-  btnAdd.addEventListener('click', function(e) {
-    addTodo();
-  })
-  
-function renderTodo() {
-  var getListToDo = JSON.parse(localStorage.getItem('todos'));
-  var html = '';
-  if(getListToDo) {
-    getListToDo.forEach(function(key) {
-      html += "<li class='todo-content'>" + 
-      "<p>"+key.text+"</p>" +
-      "<button id="+key.id+" class='btn btn-primary js-btn-delete'>" +
-        "<i class='fa-solid fa-trash'></i>" +
-      "</button>" +
-      "</li>"
-    })
-    document.querySelector('.list-todo').innerHTML = html;
-  }
-  var btnDelete = document.querySelectorAll('.js-btn-delete')
-  btnDelete.forEach(function(item){
-    item.addEventListener('click', function(e) {
-      removeTodo(e.target.id);
-    })
-  })    
-}
+btnAdd.addEventListener('click', function(e) {
+  addTodo();
+})
 
 function addTodo() {
   var getContent = document.querySelector('.text-todo').value;
-  var getStorageItem = localStorage.getItem('todos');
-  var listTodo = JSON.parse(getStorageItem) || [];
-  if (listTodo) {
-    listTodo.push({
-      id: Date.now(),
-      text: getContent
-    });
-    localStorage.setItem('todos', JSON.stringify(listTodo));
-    renderTodo();
+  var storageItem = localStorage.getItem('todos');
+  var listTodo = JSON.parse(storageItem) || [];
+  var todoItem = {
+    id: Date.now(),
+    text: getContent
   }
+  if (listTodo) {
+    listTodo.push(todoItem);
+  }
+    localStorage.setItem('todos', JSON.stringify(listTodo));
+    renderTodoItem(todoItem);
+  }
+
+function renderTodoItem(listTodo) {
+  var itemTodo = document.createElement('li');
+  itemTodo.classList.add('item-todo');
+  var getIdRemove = listTodo.id;
+  var contentTodo = document.createElement('p');
+  contentTodo.classList.add('content-todo');
+  contentTodo.innerText = listTodo.text;
+  var removeBtnTodo = document.createElement('button');
+  var listTodo = document.querySelector('.list-todo');
+  removeBtnTodo.classList.add('btn', 'btn-primary', 'js-btn-remove-');
+  removeBtnTodo.setAttribute('id',getIdRemove);
+  removeBtnTodo.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  removeBtnTodo.addEventListener('click', function() {
+    removeTodo(this);
+  })
+  listTodo.appendChild(itemTodo);
+  itemTodo.appendChild(contentTodo);
+  itemTodo.appendChild(removeBtnTodo);
 }
 
-function removeTodo(id) {
-  var getStorageItem = localStorage.getItem('todos');
-  var listTodo = JSON.parse(getStorageItem) || [];
-  listTodo.map(function(key, index){
-    if(key.id === +id) {
-      listTodo.splice(index, 1);
-    }
+function renderTodo() {
+  var listTodo = JSON.parse(localStorage.getItem('todos'));
+    listTodo.forEach(function(item) {
+      renderTodoItem(item);
+    });
+  }  
+
+function removeTodo(btnDelete) {
+  var id = btnDelete.id;
+  var storageItem = localStorage.getItem('todos');
+  var listTodo = JSON.parse(storageItem) || [];
+  var index = listTodo.findIndex(function(item) {
+    return item.id == id;
   })
+  btnDelete.parentElement.remove();
+  listTodo.splice(index, 1);
   localStorage.setItem('todos', JSON.stringify(listTodo));
-  renderTodo();
 }
 
 renderTodo();
